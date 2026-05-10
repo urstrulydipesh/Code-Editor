493365ed776d46cfbdc97a44a673721a3d071102
urstrulydipesh <dipesh.shah546@gmail.com>
2026-05-11 02:54:24 +0530

    Payment fixed

--- src/app/pricing/_components/UpgradeButton.tsx
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function UpgradeButton() {
   const CHEKOUT_URL =
-    "https://ytprogrammingstore.lemonsqueezy.com/buy/d459dddb-a233-4060-9e72-90a1a7740552";
+    "https://codecraftexample.lemonsqueezy.com/checkout/buy/b8a57f8a-8678-427d-83fe-321c14b84790";
 
   return (
     <Link
